bbcdee508282921818121e645acc10a239b1448c
park changyong <ridebus82@nate.com>
2024-12-31 03:26:01 +0900

    241231 asdfljgogogogo

--- back-lib/lib.js
+++ back-lib/lib.js
@@ -60,3 +60,36 @@ export function deleteFolder(folderPath) {
         console.log(`${targetPath} 폴더가 존재하지 않습니다.`);
     }
 }
+
+
+export function mailSender(reciever, subject, content) {
+
+    var transporter = nodemailer.createTransport({
+        service: 'naver',   // 메일 보내는 곳
+        prot: 465,
+        host: 'smtp.naver.com',
+        secure: false,
+        requireTLS: true,
+        auth: {
+            user: process.env.N_MAIL_ID,
+            pass: process.env.N_MAIL_PWD
+        }
+    });
+    // 메일 옵션
+    var mailOptions = {
+        from: `${process.env.N_MAIL_ID}@naver.com`, // 보내는 메일의 주소
+        to: reciever, // 수신할 이메일
+        subject: subject, // 메일 제목
+        text: content // 메일 내용
+    };
+
+    // 메일 발송    
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            // console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
+
