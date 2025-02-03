f9417eeae21266512161f5e22c7616c81511ea23
rider <ridebus82@nate.com>
2025-02-03 11:31:46 +0900

    250203 aslfkdjasdfogogogo

--- back-lib/db.js
+++ back-lib/db.js
@@ -18,7 +18,7 @@ ALTER TABLE hy_site MODIFY COLUMN hy_main_image VARCHAR(255);
 ALTER TABLE hy_site MODIFY COLUMN hy_card_image VARCHAR(255);
 
 
-ALTER TABLE land ADD COLUMN ld_event_img VARCHAR(255) AFTER ld_popup_img;
+ALTER TABLE land ADD COLUMN ld_mobile_bt_event_img VARCHAR(255) AFTER ld_popup_img;
 ld_mobile_bt_phone_img
 ld_mobile_bt_event_img
 ld_event_img
