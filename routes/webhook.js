9e46a83b8e06f2d52df24558ee1a4d03cc5e76ca
rider <ridebus82@nate.com>
2025-03-14 15:25:36 +0900

    250314 asldjfasdofoggogogoo

--- routes/webhook.js
+++ routes/webhook.js
@@ -163,7 +163,7 @@ webhookRouter.post('/', async (req, res) => {
             getLeadsData = leadRes.data;
             console.log(getLeadsData);
         } catch (err) {
-            console.log(err);
+            console.log(err.errors);
             console.log('raidsed error lead url?!?!?!?!?!?!??!?!');
         }
 
@@ -185,12 +185,12 @@ webhookRouter.post('/', async (req, res) => {
 
 
         // for문 돌려서 baseData 만들기!
-        if(getLeadsData.field_data){
+        if (getLeadsData.field_data) {
 
         }
         const leadsData = getLeadsData.field_data;
 
-        
+
         let baseData = {};
         let etcCount = 0;
         for (let i = 0; i < leadsData.length; i++) {
